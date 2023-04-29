f2b575e275fda51338095151fec023cc81e0d837
Abhinav Aashish <aaashish@cisco.com>
2023-04-29 20:17:21 +0530

    Modified output

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
 	public static void main(String[] args) {
 		
 		for (int i=1;i<=10 ;i++) {
-			System.out.println(i+" Abhinav Aashish");
+			System.out.println(i+" -> Hello, Abhinav Aashish");
 		}
 	}
 }
